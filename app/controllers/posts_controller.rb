937e6ccc457e321345f7bf9eaec1261ac325a56a
David Lewis <davidlewisp@gmail.com>
2018-05-15 20:40:59 -0700

    add delete and update methods for api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
-  
+
   def index
     @posts = Post.all
     render json: @posts
@@ -10,8 +10,34 @@ class PostsController < ApplicationController
     render json: @post
   end
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+  end
+
   private
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit!
     end
 end
